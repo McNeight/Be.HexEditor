af99b0d921b3189457934f76482b338ab9f41e77
Neil McNeight <mcneight+github@gmail.com>
2018-05-25 14:34:02 -0500

    Split out nested classes to individual files for easier tracking of changes

---
+++ Be.Windows.Forms.HexBox/HexBox.EmptyKeyInterpreter.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Be.Windows.Forms
+{
+    public partial class HexBox
+    {
+        /// <summary>
+        /// Represents an empty input handler without any functionality. 
+        /// If is set ByteProvider to null, then this interpreter is used.
+        /// </summary>
+        class EmptyKeyInterpreter : IKeyInterpreter
+        {
+            HexBox _hexBox;
+
+            public EmptyKeyInterpreter(HexBox hexBox)
+            {
+                _hexBox = hexBox;
+            }
+
+            #region IKeyInterpreter Members
+            public void Activate() { }
+            public void Deactivate() { }
+
+            public bool PreProcessWmKeyUp(ref Message m)
+            { return _hexBox.BasePreProcessMessage(ref m); }
+
+            public bool PreProcessWmChar(ref Message m)
+            { return _hexBox.BasePreProcessMessage(ref m); }
+
+            public bool PreProcessWmKeyDown(ref Message m)
+            { return _hexBox.BasePreProcessMessage(ref m); }
+
+            public PointF GetCaretPointF(long byteIndex)
+            { return new PointF(); }
+
+            #endregion
+        }
+    }
+}
