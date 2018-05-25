e0db198f8a702b1f2e64ac37c0d99e2408460946
Neil McNeight <mcneight+github@gmail.com>
2018-05-25 11:56:33 -0500

    Switch NativeMethods from public to internal

--- Be.Windows.Forms.HexBox/NativeMethods.cs
+++ Be.Windows.Forms.HexBox/NativeMethods.cs
@@ -7,20 +7,20 @@ namespace Be.Windows.Forms
     {
         // Caret definitions
         [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool CreateCaret(IntPtr hWnd, IntPtr hBitmap, int nWidth, int nHeight);
+        internal static extern bool CreateCaret(IntPtr hWnd, IntPtr hBitmap, int nWidth, int nHeight);
 
         [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool ShowCaret(IntPtr hWnd);
+        internal static extern bool ShowCaret(IntPtr hWnd);
 
         [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool DestroyCaret();
+        internal static extern bool DestroyCaret();
 
         [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool SetCaretPos(int X, int Y);
+        internal static extern bool SetCaretPos(int X, int Y);
 
         // Key definitions
-        public const int WM_KEYDOWN = 0x100;
-        public const int WM_KEYUP = 0x101;
-        public const int WM_CHAR = 0x102;
+        internal const int WM_KEYDOWN = 0x100;
+        internal const int WM_KEYUP = 0x101;
+        internal const int WM_CHAR = 0x102;
     }
 }
